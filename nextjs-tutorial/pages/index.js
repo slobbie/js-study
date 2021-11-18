3b3d073bae61b0f217c1013ade5dd6a0c2abc5ba
slobbie <haeru9410@gmail.com>
2021-11-18 22:00:09 +0900

    study

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -3,44 +3,35 @@
 
 import Axios  from 'axios';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { Header, Divider, Loader } from 'semantic-ui-react';
 import ItemList from '../src/component/ItemList';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+export default function Home({list}) {
+  // const [list, setList] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  // const API_URL = process.env.NEXT_PUBLIC_API_URL;
     
 
-    function getData() {
-      Axios.get(API_URL).then((res) => {
-        console.log(res.data);
-        setList(res.data);
-        setIsLoading(false);
-      }); 
-    }
+  //   function getData() {
+  //     Axios.get(API_URL).then((res) => {
+  //       console.log(res.data);
+  //       setList(res.data);
+  //       setIsLoading(false);
+  //     }); 
+  //   }
 
-    useEffect(() => {
-       getData();
-    }, [])
+  //   useEffect(() => {
+  //      getData();
+  //   }, [])
   return ( 
     <div>
       <Head>
         <title>Home | Next.js</title>
         <meta name="description" content="NextJsHome"></meta>
-      </Head>
-      {isLoading && (
-        <div style={{ padding: "300px 0" }}>
-          <Loader inline="centered" active>
-            Loading
-          </Loader>
-        </div>
-      )}
-       {!isLoading && (
+      </Head> 
         <>
           <Header as="h3" style={{ paddingTop: 40 }}>
             베스트 상품
@@ -48,12 +39,36 @@ export default function Home() {
           <Divider />
           <ItemList list={list.slice(0, 9)} />
           <Header as="h3" style={{ paddingTop: 40 }}>
-            신상품
+          신상품
           </Header>
           <Divider />
           <ItemList list={list.slice(9)} />
         </>
-      )}
+
+
+           {/* {isLoading && (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      )} */}
+       {/* {!isLoading && (
+        
+      )} */}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const apiUrl = process.env.apiUrl;
+  const res = await Axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props: {
+      list: data,
+      name: process.env.name,
+    },
+  };
 }
