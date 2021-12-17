cd698da9aa942a37045387ad9656b6ac5009def6
slobbie <haeru9410@gmail.com>
2021-12-17 23:39:47 +0900

    study

---
+++ react stack/slider/slide.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Slide({ img }) {
+  return (
+      <IMG src={img} />
+  );
+}
+// const IMG = styled.img`
+//   width: 100%;
+//   height: 70vh;
+// `;
